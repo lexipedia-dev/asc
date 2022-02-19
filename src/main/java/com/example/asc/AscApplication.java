972aa812ebb1b4f819a5d0bf34bffc6bdc0dccd4
Alexsander <alexsander.mviana@gmail.com>
2022-02-19 01:15:08 -0300

    Criação de properties para Dev e Homolog. Criação de arquivo json para configurações de @Values Criação do AscApplication para que inicie os @Values

--- src/main/java/com/example/asc/AscApplication.java
+++ src/main/java/com/example/asc/AscApplication.java
@@ -1,13 +1,26 @@
 package com.example.asc;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class AscApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AscApplication.class, args);
+    }
+
+    @Component
+    public static class Runner implements ApplicationRunner{
+
+        @Override
+        public void run(ApplicationArguments args) throws Exception {
+
+        }
     }
 
 }
